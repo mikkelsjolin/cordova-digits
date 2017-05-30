d56f1d59bd11389205c48328f14844d230ba3cd4
Mikkel Sjølin <m.sjolin@gmail.com>
2017-05-30 13:06:09 +0200

    Downgrade

--- www/CordovaDigits.js
+++ www/CordovaDigits.js
@@ -1,23 +1,21 @@
 var exec = require('cordova/exec');
 
-exports.authenticate = function(options, success, error) {
-    exec(success, error, 'CordovaDigits', 'authenticate', [options]);
+var CordovaDigits = {
+	authenticate: function(options, success, error) {
+	    exec(success, error, 'CordovaDigits', 'authenticate', [options]);
+	},
+	logOut = function(success, error) {
+	    exec(success, error, 'CordovaDigits', 'logOut', []);
+	},
+	isAuthenticated: function(success, error) {
+	    exec(success, error, 'CordovaDigits', 'isAuthenticated', []);
+	},
+	onSessionHasChanged: function(success, error) {
+	    exec(success, error, 'CordovaDigits', 'onSessionHasChanged', []);
+	},
+	onSessionExpired: function(success, error) {
+	    exec(success, error, 'CordovaDigits', 'onSessionExpired', []);
+	}
 };
 
-exports.logOut = function(success, error) {
-    exec(success, error, 'CordovaDigits', 'logOut', []);
-};
-
-exports.isAuthenticated = function(success, error) {
-    exec(success, error, 'CordovaDigits', 'isAuthenticated', []);
-};
-
-exports.onSessionHasChanged = function(success, error) {
-    exec(success, error, 'CordovaDigits', 'onSessionHasChanged', []);
-};
-
-exports.onSessionExpired = function(success, error) {
-    exec(success, error, 'CordovaDigits', 'onSessionExpired', []);
-};
-
-
+module.exports = CordovaDigits;
